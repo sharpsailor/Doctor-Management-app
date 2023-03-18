fe9642e12126ddaf1701f364c1024efdcbb99105
sharpsailor <sharpsailor13@gmail.com>
2023-03-19 01:52:24 +0530

    Login auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "../styles/RegisterStyles.css";
-import { Form, Input } from "antd";
-import { Link } from "react-router-dom";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const Login = () => {
-  const onfinishHandler = (values) => {
-    console.log(values);
+  const navigate = useNavigate();
+  const onfinishHandler = async (values) => {
+    try {
+      const res = await axios.post("/api/v1/user/Login", values);
+      if (res.data.success) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successfully");
+        navigate("/");
+      } else {
+        message.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Something went wrong");
+    }
   };
   return (
     <>
