1ad0d0bdd5366fcf2152c8b863b10b42601e258b
sharpsailor <sharpsailor13@gmail.com>
2023-03-19 18:13:14 +0530

    Login Functionality

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const HomePage = () => {
+  // Login USer data
+  const getUserData = async () => {
+    try {
+      const res = await axios.post(
+        "./api/v1/user/getUserData",
+        {},
+        {
+          headers: {
+            Authorization: "Bearer" + localStorage.getItem("token"),
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
   return (
     <div>
       <h1>Home Page</h1>
